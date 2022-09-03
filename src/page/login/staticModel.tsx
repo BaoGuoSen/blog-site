88163d86ea92710504cc33eeaefa265a84b12edb
YCR <744765302@qq.com>
2022-09-03 19:00:48 +0800

    chore: 微调权限

--- src/page/login/staticModel.tsx
+++ src/page/login/staticModel.tsx
@@ -7,13 +7,14 @@ const loginForm: IComponent[] = [
     label: '账户：',
     name: 'username',
     require: true,
-    rules: ['word'],
+    rules: ['int'],
     range: [6, 12],
     placeholder: '请输入账户名'
   },
   {
     label: '密码：',
     name: 'password',
+    range: [6, 18],
     require: true,
     placeholder: '请输入登录密码',
     element: <Input.Password />
